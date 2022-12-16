8c31855310590dbab8aaeab1c525bd637f8db3ef
unknown <viczik16@gmail.com>
2022-12-16 15:04:42 +0100

    modified:   cbt/settings.py2

--- cbt/settings.py
+++ cbt/settings.py
@@ -41,7 +41,7 @@ DEBUG = True
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
